d0c010b5809216596b51ac7ed82012cb90e34d92
Adam Griffith <5164766+adamjgriffith@users.noreply.github.com>
2023-10-16 08:29:53 +0100

    Setup passport

---
+++ docker/mysql/init/databases.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE authorisation;
+GRANT ALL PRIVILEGES ON authorisation.* TO `laravel`@`%`;
+
+CREATE DATABASE resource;
+GRANT ALL PRIVILEGES ON resource.* TO `laravel`@`%`;
--- resource-server/routes/web.php
+++ resource-server/routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Str;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +18,67 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/redirect', function (Request $request) {
+    $request->session()->put('state', $state = Str::random(40));
+
+    $request->session()->put(
+        'code_verifier', $code_verifier = Str::random(128)
+    );
+
+    $codeChallenge = strtr(rtrim(
+        base64_encode(hash('sha256', $code_verifier, true))
+        , '='), '+/', '-_');
+
+    // php artisan passport:client --public
+    //
+    // Which user ID should the client be assigned to? (Optional):
+    // >
+    //
+    // What should we name the client?:
+    // >
+    //
+    // Where should we redirect the request after authorization? [http://localhost/auth/callback]:
+    // > http://localhost:8001/auth/callback
+    //
+    // New client created successfully.
+    // Client ID:
+    // Client secret:
+
+    $query = http_build_query([
+        'client_id' => '1',
+        'redirect_uri' => config('app.url').'/auth/callback',
+        'response_type' => 'code',
+        'scope' => '',
+        'state' => $state,
+        'code_challenge' => $codeChallenge,
+        'code_challenge_method' => 'S256',
+        // 'prompt' => '', // "none", "consent", or "login"
+    ]);
+
+    return redirect('http://localhost:8000/oauth/authorize?'.$query);
+});
+
+Route::get('/auth/callback', function (Request $request) {
+    $state = $request->session()->pull('state');
+
+    $codeVerifier = $request->session()->pull('code_verifier');
+
+    // dd($request->session());
+
+    throw_unless(
+        strlen($state) > 0 && $state === $request->state,
+        InvalidArgumentException::class
+    );
+
+    $response = Http::asForm()->post('http://localhost:8000/oauth/token', [
+        'grant_type' => 'authorization_code',
+        'client_id' => 'client-id',
+        'redirect_uri' => config('app.url').'/auth/callback',
+        'code_verifier' => $codeVerifier,
+        'code' => $request->code,
+    ]);
+
+    return $response->json();
 });
